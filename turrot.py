5fb263826133736389ed0e529743f57cb4398aaf
Fandi63 <100903182+Fandi63@users.noreply.github.com>
2023-02-23 19:57:21 +0100

    Add files via upload

---
+++ turrot.py
@@ -0,0 +1,12 @@
+import turtle as tr
+
+
+def rotate_right(forward, right):
+    while True:
+        tr.forward(forward)
+        tr.right(right)
+
+def rotate_left(forward2, left):
+    while True:
+        tr.forward(forward2)
+        tr.left(left)
